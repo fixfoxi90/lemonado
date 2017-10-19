c6f2513f27c29fd5a8eee6a901991d14ac524369
student <felix.steidler@smi.ag>
2017-10-19 22:29:21 +0200

    added stuff

---
+++ core/Lemonado/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lemonado;
+
+
+class Application
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
-var_dump($_SERVER['APPLICATION_ENV']); die;
-
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
 
+define('APPLICATION_ENV', $_SERVER['APPLICATION_ENV']);
+
 // Composer autoloading
 include __DIR__ . '/../vendor/autoload.php';
+
+$application = new \Lemonado\Application();
+
+var_dump($application); die;
 
